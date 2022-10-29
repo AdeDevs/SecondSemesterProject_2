b1354bbff664ed3c3f276591f50acb6c06892b96
Adeyemi Akinyemi <89089354+AdeDevs@users.noreply.github.com>
2022-10-29 02:37:45 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,14 +8,14 @@ function App() {
   return (
     <div>
         <Routes>
-          <Route path="/home" element={<HomePage />} />
+          <Route path="/" element={<HomePage />} />
           
           <Route path="/error" element={<MyApp />} />
 
           <Route path="/*" element={<Error404 />} />
         </Routes>
 
-        <Link to="/home"><button style={{marginTop:"20px", marginRight:"20px"}}>Prev</button></Link>
+        <Link to="/"><button style={{marginTop:"20px", marginRight:"20px"}}>Prev</button></Link>
         <Link to="/error"><button style={{marginTop:"20px"}}>Next</button></Link>
     </div>
   );
